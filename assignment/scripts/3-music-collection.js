6fc943c32a0ed35d11298d725ead1d258dffb3a1
Courtney Azar <courtney.a.azar@gmail.com>
2022-04-25 15:07:30 -0500

    added all 6 albums to the 'collection'

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -14,3 +14,5 @@ console.log(addToCollection("Hot Fuss", "The Killers", 2004));
 console.log(addToCollection("Franz Ferdinand", "Franz Ferdinand", 2004));
 console.log(addToCollection("The Joshua Tree", "U2", 1987));
 console.log(addToCollection("Black Holes and Revelations", "Muse", 2006));
+console.log(addToCollection("The Bends", "Radiohead", 1995));
+console.log(addToCollection("The Blue Album", "Weezer", 1994));
