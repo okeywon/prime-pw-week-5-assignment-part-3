d19c7f1a27abfaab293a372f8f25a7f9ca1a50e8
Courtney Azar <courtney.a.azar@gmail.com>
2022-04-25 15:03:38 -0500

    created function to add items to object 'collection'

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,3 +1,16 @@
 console.log('***** Music Collection *****')
 
-let collection = [];
+let collection = {
+    album: '',
+    artist: '',
+    year: 0
+}
+
+function addToCollection( album, artist, year ) {
+    return collection += [album, artist, year];
+}
+
+console.log(addToCollection("Hot Fuss", "The Killers", 2004));
+console.log(addToCollection("Franz Ferdinand", "Franz Ferdinand", 2004));
+console.log(addToCollection("The Joshua Tree", "U2", 1987));
+console.log(addToCollection("Black Holes and Revelations", "Muse", 2006));
