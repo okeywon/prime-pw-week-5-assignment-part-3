e7b1202b7b7b788e2e1f3da6b1bda77fb09044b2
Courtney Azar <courtney.a.azar@gmail.com>
2022-04-27 14:53:38 -0500

    not sure what I'm doing with the showCollection function yet

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,18 +1,33 @@
 console.log('***** Music Collection *****')
 
 let collection = {
-    album: '',
+    title: '',
     artist: '',
     year: 0
 }
 
-function addToCollection( album, artist, year ) {
-    return collection += [album, artist, year];
+function addToCollection( title, artist, year ) {
+    return collection += [title, artist, year, "\n"];
 }
 
-console.log(addToCollection("Hot Fuss", "The Killers", 2004));
-console.log(addToCollection("Franz Ferdinand", "Franz Ferdinand", 2004));
-console.log(addToCollection("The Joshua Tree", "U2", 1987));
-console.log(addToCollection("Black Holes and Revelations", "Muse", 2006));
-console.log(addToCollection("The Bends", "Radiohead", 1995));
-console.log(addToCollection("The Blue Album", "Weezer", 1994));
+(addToCollection("Hot Fuss", "The Killers", 2004));
+(addToCollection("Franz Ferdinand", "Franz Ferdinand", 2004));
+(addToCollection("The Joshua Tree", "U2", 1987));
+(addToCollection("Black Holes and Revelations", "Muse", 2006));
+(addToCollection("The Bends", "Radiohead", 1995));
+(addToCollection("The Blue Album", "Weezer", 1994));
+
+console.log(collection);
+
+function showCollection(array) {
+    let title = array.title;
+    let artist = array.artist;
+    let year = array.year;
+    let i = 0;
+    for (items in array) {
+        console.log(`${title} by ${artist}, published in ${year}`);
+        i++;
+    }   return array;
+} 
+
+console.log(showCollection(collection));
