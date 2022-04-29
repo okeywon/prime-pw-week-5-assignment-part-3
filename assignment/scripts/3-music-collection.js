118f9036dee3646142235745a0c00fdb61b123c3
Courtney Azar <courtney.a.azar@gmail.com>
2022-04-28 22:24:12 -0500

    updated comments and formatting. Added a console log to show partial queries for search function.

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -2,19 +2,20 @@ console.log('***** Music Collection *****')
 
 let collection = [];
 
-// Update "addToCollection" to also take in parameter tracks.
+// function to add items to collection array.
+// takes in string items for title(album name), artist, yearPublished, name(song), duration(of song).
+// returns the collection array.
 function addToCollection(title, artist, yearPublished, name, duration) {
-    let album = { // creates an object album with parameters of title artist and yearPublished
+    let album = {
         title: title,
         artist: artist,
         yearPublished: yearPublished,
         tracks: {name: name, duration: duration}
     }
-    collection.push(album); // pushes album items into collection array
+    collection.push(album);
     return collection;
 }
 
-// these conosle logs add the parameters requeseted in addToCollection function to the collection array
 console.log(addToCollection("Hot Fuss", "The Killers", "2004", 'Mr. Brightside', '3:43'));
 console.log(addToCollection("Franz Ferdinand", "Franz Ferdinand", "2004", 'Take Me Out', '3:57'));
 console.log(addToCollection("The Joshua Tree", "U2", "1987", 'With or Without You', '4:56'));
@@ -22,57 +23,57 @@ console.log(addToCollection("Black Holes and Revelations", "Muse", "2006", 'Star
 console.log(addToCollection("The Bends", "Radiohead", "1995", 'My Iron Lung', '4:36'));
 console.log(addToCollection("The Blue Album", "Weezer", "1994", "My Name is Jonas", "3:24"));
 
-console.log(collection); // logs the completed collection
+console.log(collection);
 
-// function to show the collection array in a more pretty fashion than just console logging it. Using a global/reusable array.
-// Update "showCollection" to display the list of tracks for each album with its "name" & "duration".
+// function to show the collection array in a more pretty fashion. Parameter is a global array.
+// Prints array length and each album in the collection array to the console.
 function showCollection(array) {
-    console.log(array.length); // prints array length to console.
-    for (items of array) { // for items inside a global array
+    console.log(array.length);
+    for (items of array) {
         console.log(`${items.title} by ${items.artist}, published in ${items.yearPublished}: \n Song: ${items.tracks.name}, Duration: ${items.tracks.duration}.`);
-        // prints album info to console -> tracks info is not printing correctly YET.
     }
 } 
 
-(showCollection(collection)); // shows collection array as dictated on line 35
+(showCollection(collection));
 
-// function to search a particular artist
+// function to search for a particular artist in the collection array.
+// takes in a string for the artist name.
+// returns the results array which should print a string of the correct artist if found, or an empty array if not found.
 function findByArtist(artist) {
-    let results = []; // empty array to push artist into
-    for (let i = 0; i < collection.length; i++) { // loops through the entire collection
-        if (artist === collection[i].artist) { // if the artist queried is equal to the collection.artist index
-            results.push(collection[i].artist); // the queried artist will be pushed from the collection.artist to the results array
-            return results; // returns results array
+    let results = [];
+    for (let i = 0; i < collection.length; i++) {
+        if (artist === collection[i].artist) {
+            results.push(collection[i].artist);
+            return results;
         }
-    } return []; // otherwise it will return an empty array
+    } return [];
 }
 
-console.log(findByArtist('U2')); // shows that this will console log a correct item
-console.log(findByArtist('Pink')); // shows that this will log a blank array if incorrect
+console.log(findByArtist('U2'));
+console.log(findByArtist('Pink'));
 
-// Update search to include "trackName"
+// function to search the collection array by name(artist name - string), year(string), and trackName(song name - string)
+// returns the answer array which will only include items located. Will not print items not located. Will print partial queries if they exist in collection!
 function search(name, year, trackName) { // search function that takes three parameters name, year, song.
-    let answer = []; // empty array to push answers into only if correct.
-    // variable to push answers into and console log
-    for (let i = 0; i < collection.length; i++) { // loops through the entire collection
-        if (name === collection[i].artist) { // if name queried is the same as collection.artist index
-            answer.push(collection[i].artist); // pushes the correct artist into the answer array to be logged at the return
-        } // another if statement to ensure the loop continues as above
-        if (year === collection[i].yearPublished) { // if year queried is the same as a year in Collection array
-            answer.push(collection[i].yearPublished); // yearPublished from collection array is pushed into answer to be logged at return
-        } // another if statement to ensure loop continues
-        if (trackName === collection[i].tracks.name) { // if song queried is the same as the collection.name index
-            answer.push(collection[i].tracks.name); // correct song name is pushed into answer array
-        } // multiple if statements works better in this case than several else if's as that stops the loop and I don't want to do that.
-         // double else if in this case since I only want these to run if all the above if statements DON'T run.
-        else if (name != collection[i].artist) { // if the above if statements are false and name queried doesn't equal a collection.artist index
-            answer.push(); // pushes nothing into answer array
+    let answer = [];
+    for (let i = 0; i < collection.length; i++) {
+        if (name === collection[i].artist) {
+            answer.push(collection[i].artist);
         }
-        else if (year != collection[i].yearPublished) { // if above statments are false and year queried doesn't equal collection.year index
-            answer.push(); // pushes nothing into answer array
+        if (year === collection[i].yearPublished) {
+            answer.push(collection[i].yearPublished);
         }
-    }   return answer; // returns answer outside my loop to ensure loop runs through all parameters before ending.
+        if (trackName === collection[i].tracks.name) {
+            answer.push(collection[i].tracks.name);
+        }
+        else if (name != collection[i].artist) {
+            answer.push();
+        }
+        else if (year != collection[i].yearPublished) {
+            answer.push();
+        }
+    }   return answer;
 }
 
-console.log(search('U2', '1987', 'With or Without You')); // console logs only correct items if located in Collection as expected.
-// will not console log incorrect queries. Only returns queried items that exist in collection array. 
+console.log(search('U2', '1987', 'With or Without You'));
+console.log(search('White Stripes', '1995', 'Pinball Wizard'));
